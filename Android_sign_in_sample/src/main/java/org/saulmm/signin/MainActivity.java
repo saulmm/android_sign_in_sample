0195e9804a607442075072601efd5ab809ace9f9
Saul Molinero <saulmm2@gmail.com>
2013-10-20 16:21:50 +0200

    Added google_play_services_lib and gradle configuration

--- Android_sign_in_sample/src/main/java/org/saulmm/signin/MainActivity.java
+++ Android_sign_in_sample/src/main/java/org/saulmm/signin/MainActivity.java
@@ -1,7 +1,7 @@
 package org.saulmm.signin;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		
     }
 
 
